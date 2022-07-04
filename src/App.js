4bf8ad3855448df367e95c4af2bd919486cd49e9
Nguyen Thanh Do <dontce150457@fpt.edu.vn>
2022-07-04 15:35:16 +0700

    change App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         skjf;ajf 'qj wfqEFJ EFJ '
         </a>
       </header>
     </div>
