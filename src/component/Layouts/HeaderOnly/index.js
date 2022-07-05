8213909c6b5c4d309adeac4fb5f2656f04fa99a0
Nguyen Thanh Do <dontce150457@fpt.edu.vn>
2022-07-05 14:56:57 +0700

    build Layout, router

---
+++ src/component/Layouts/HeaderOnly/index.js
@@ -0,0 +1,2 @@
+
+export {default as HeaderOnly} from  './HeaderOnly'
---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import Home from "~/pages/Home";
+import Following from "~/pages/Following";
+import Profile from "~/pages/Profile";
+import Search from "~/pages/Search";
+import Upload from "~/pages/Upload";
+import { HeaderOnly } from "~/component/Layouts/HeaderOnly";
+
+// import HeaderOnly from '~/layouts/component/HeaderOnly';
+
+const publicRoutes =[
+    {path:'/', component : Home},
+    {path:'/following', component : Following},
+    {path:'/profile', component : Profile},
+    {path:'/search', component : Search, layout:null},
+    {path:'/upload', component : Upload, layout: HeaderOnly},
+]
+const privateRoutes = []
+export { publicRoutes, privateRoutes };
